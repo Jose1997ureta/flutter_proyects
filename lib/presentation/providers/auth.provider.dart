2fda6c1cd3fa85753bd547734094a8494baa6afc
Jose Ureta <jose.ureta@onroadts.com>
2025-01-04 21:19:12 -0500

    Se agrego la configuracion para java17

--- lib/presentation/providers/auth.provider.dart
+++ lib/presentation/providers/auth.provider.dart
@@ -1,6 +1,6 @@
 // class Auth
 import 'package:flutter/material.dart';
-// import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthProvider extends ChangeNotifier {
   bool _isLoggedIn = false;
@@ -9,27 +9,27 @@ class AuthProvider extends ChangeNotifier {
   bool get isLoggedIn => _isLoggedIn;
   String get token => _token;
 
-  // Future<void> validateIsAuth() async {
-  //   await Future.delayed(const Duration(seconds: 2));
+  Future<void> validateIsAuth() async {
+    await Future.delayed(const Duration(seconds: 2));
 
-  //   final pref = await SharedPreferences.getInstance();
-  //   pref.getString("token");
-  //   pref.getString("userName");
-  //   pref.getString("email");
-  //   pref.getBool("isLoggedIn");
-  // }
+    final pref = await SharedPreferences.getInstance();
+    pref.getString("token");
+    pref.getString("userName");
+    pref.getString("email");
+    pref.getBool("isLoggedIn");
+  }
 
   Future<void> login(String email, String password) async {
     if (email == "jose@gmail.com" && password == "123456") {
       _isLoggedIn = true;
       _token = "jose123456";
 
-      // final pref = await SharedPreferences.getInstance();
+      final pref = await SharedPreferences.getInstance();
 
-      // await pref.setString("token", _token);
-      // await pref.setString("userName", "Jose");
-      // await pref.setString("email", "jose@gmail.com");
-      // await pref.setBool("isLoggedIn", true);
+      await pref.setString("token", _token);
+      await pref.setString("userName", "Jose");
+      await pref.setString("email", "jose@gmail.com");
+      await pref.setBool("isLoggedIn", true);
 
       notifyListeners();
     } else {
